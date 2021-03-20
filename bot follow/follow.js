d8bc0a6e030ae5ab8a4fd9755045d0d59739a722
luis-fernand <fernando.r_138@hotmail.com>
2021-03-20 18:03:31 -0400

    teste do/while + loop randomico
    como não consegui fazer rodar com o for, vou voltar uns passos e tentar com o do/while...
    em paralelo a isso preciso descobrir uma forma de fazer loops randomicamente

--- bot follow/follow.js
+++ bot follow/follow.js
@@ -3,13 +3,16 @@ O objetivo não é abusar desse recurso, mas sim facilitar a vida do usuário...
 Por esse motivo adicionei um limite (30) através do for (estou aprendendo a usar esse recurso)
 Assim como a função setTimeout é para não ser penalisado pelo sistema.
 */
-for (let contador = 0; contador < 2; contador++) {
+var contador = 0
+do {
     document.querySelectorAll('.sqdOP.L3NKy.y3zKF').forEach((item, index) => {
         setTimeout(() => {
             if(!item.classList.contains('._8A5w5')) {
                 item.click();
+                contador++;
                 console.log(`Já seguiu ${contador} pessoas.`);
             }
-        }, index * 10000);
+        }, index * (Math.floor(Math.random() * 50000) + 10000));
     });
 }
+while (contador < 3)
