21175ed8ce1366b73637efd50276df0e6790601d
luis-fernand <fernando.r_138@hotmail.com>
2021-03-21 13:29:16 -0400

    att teste

--- bot follow/follow-teste.js
+++ bot follow/follow-teste.js
@@ -1,8 +1,14 @@
 let contador = 0
 let buttons = document.querySelectorAll('.L3NKy')
 
-buttons.forEach((v, i) => {
+buttons.forEach((botao, index) => {
     setTimeout(() => {
-        
-    }, )
+        if(!botao.classList.contains('_8A5w5')){
+            botao.click()
+            contador++
+            console.log(`Já seguiu ${contador} pessoas.`)
+        } else {
+            console.log('Você já segue essa pessoa.')
+        }
+    }, index * (Math.floor(Math.random() * 5000) + 1000)); 
 })
